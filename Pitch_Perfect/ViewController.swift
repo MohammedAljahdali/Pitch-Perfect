d815642563d230a648520f94b18123d8e0f8b92e
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-06 02:36:04 +0300

    buttons outlet

--- Pitch_Perfect/ViewController.swift
+++ Pitch_Perfect/ViewController.swift
@@ -10,20 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        stopRecordingButton.isEnabled = false
+        
     }
 
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
+        
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         print("stop recording")
         recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordingButton.isEnabled = true
     }
 }
 
